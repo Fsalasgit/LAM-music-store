4b093f567d762becf20a2934a2ab96151262fcef
BrunoBusnelli <bbusnelli1@gmail.com>
2023-09-29 16:00:24 -0300

    agregado modal

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProductTable from '../components/Admin/ProductTable'
+import ModalNuevo from '../components/Admin/ModalNuevo'
 
 const Admin = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
   return (
     <>
     <div className="container">
         <div className="row mt-5">
             <div className="col-12">
                 <h1 className='mb-4'>Administracion</h1>
-                <button className='btn btn-primary'>Agregar producto</button>
+                <button className='btn btn-primary' onClick={handleShow}>Agregar producto</button>
                 <hr />
             </div>
         </div>
@@ -20,6 +25,7 @@ const Admin = () => {
         <div className="row">
              <ProductTable/>
         </div>
+        <ModalNuevo show={show} handleClose={handleClose} />
     </div>
     </>
   )
