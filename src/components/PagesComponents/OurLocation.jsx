e7c20d3c9652569689ecc9f2f51a09ff6e5f686d
Franco Exequiel Salas <franco_10_65@hotmail.com>
2023-10-01 19:33:24 -0300

    Antes de incrustar react leat left

---
+++ src/components/PagesComponents/OurLocation.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const OurLocation = () => {
+  return (
+    <>
+    <Container>
+        <Row>
+            <Col>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3560.1023709809247!2d-65.20977672520274!3d-26.83669609003066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sar!4v1696199240527!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </Col>
+            <Col>
+                
+            </Col>
+        </Row>
+
+    </Container>
+    </>
+  )
+}
+
+export default OurLocation
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import CarouselHome from '../components/PagesComponents/CarouselHome'
 import ShippingType from '../components/PagesComponents/ShippingType'
 import FeaturedCarousel from '../components/PagesComponents/FeaturedCarousel'
+import OurLocation from '../components/PagesComponents/OurLocation'
 
 
 
@@ -14,6 +15,12 @@ const Home = () => {
       <hr />
       <h2 className="featuredProducts__title">Productos destacados</h2>
       <FeaturedCarousel />
+    </div>
+    <div className="ourLocation">
+      <hr />
+      <h2 className="ourLocation__title">Donde encontrarnos</h2>
+      <OurLocation />
+      
     </div>
 
 
