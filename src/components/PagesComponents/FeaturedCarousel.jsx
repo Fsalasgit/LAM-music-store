47e63e5bdfc789b0be981f3b4eabc9635302777c
LorenzoAlbornoz <lorenzoa.28.la@gmail.com>
2023-10-08 19:54:09 -0300

    Productos destacados

--- src/components/PagesComponents/FeaturedCarousel.jsx
+++ src/components/PagesComponents/FeaturedCarousel.jsx
@@ -1,42 +1,49 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import FeaturedCards from './FeaturedCards';
+import { axiosInstance } from '../../config/axiosInstance';
+
 
 const FeaturedCarousel = () => {
   const [startIndex, setStartIndex] = useState(0);
+  const [featuredProducts, setFeaturedProducts] = useState([]);
 
   const handleSelect = (selectedIndex) => {
-    setStartIndex(selectedIndex * 3); // Mostrar el conjunto correspondiente
+    setStartIndex(selectedIndex);
   };
 
-  return (
-    <>
-      <Carousel id='carouselCards' activeIndex={startIndex / 3} onSelect={handleSelect} className='mt-1'>
-        <Carousel.Item>
-          <div className="d-flex justify-content-around">
-            <FeaturedCards price="1" url="https://www.heavenimagenes.com/heavencommerce/0c5cacb8-dfbd-471a-ac7e-5c09a26f6da9/images/v2/YAMAHA/21650_small.jpg"/>
-            <FeaturedCards price="2" url="https://www.heavenimagenes.com/heavencommerce/0c5cacb8-dfbd-471a-ac7e-5c09a26f6da9/images/v2/FENDER/17600_small.jpg" />
-            <FeaturedCards price="3" url="https://www.heavenimagenes.com/heavencommerce/0c5cacb8-dfbd-471a-ac7e-5c09a26f6da9/images/v2/YAMAHA/8037_small.jpg" />
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="d-flex justify-content-around">
-            <FeaturedCards price="4" url="https://www.heavenimagenes.com/heavencommerce/0c5cacb8-dfbd-471a-ac7e-5c09a26f6da9/images/v2/MUSEDO/11110_small.jpg"/>
-            <FeaturedCards price="5" url="https://www.heavenimagenes.com/heavencommerce/0c5cacb8-dfbd-471a-ac7e-5c09a26f6da9/images/v2/CAMPANA/1110141704354771_01_small.jpg"/>
-            <FeaturedCards price="6" url="https://www.heavenimagenes.com/heavencommerce/0c5cacb8-dfbd-471a-ac7e-5c09a26f6da9/images/v2/FENDER/17600_small.jpg"/>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="d-flex justify-content-around">
-            <FeaturedCards price="7" url="https://www.heavenimagenes.com/heavencommerce/0c5cacb8-dfbd-471a-ac7e-5c09a26f6da9/images/v2/YAMAHA/10698_small.jpg"/>
-            <FeaturedCards price="8" url="https://www.heavenimagenes.com/heavencommerce/0c5cacb8-dfbd-471a-ac7e-5c09a26f6da9/images/v2/CASIO/1712231809507673_01_small.jpg"/>
-            <FeaturedCards price="9" url="https://www.heavenimagenes.com/heavencommerce/0c5cacb8-dfbd-471a-ac7e-5c09a26f6da9/images/v2/CASIO/1712231809507673_01_small.jpg"/>
-          </div>
-        </Carousel.Item>
-      </Carousel>
-    </>
-  );
-}
+  useEffect(() => {
+    const fetchFeaturedProducts = async () => {
+      try {
+        const resp = await axiosInstance.get("/products");
+        const allProducts = resp.data.products;
+        const featuredProducts = allProducts.filter(product => product.isFeatured === true);
+        setFeaturedProducts(featuredProducts);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
+    fetchFeaturedProducts();
+  }, []);
+
+  const productsChunks = [];
+  for (let i = 0; i < featuredProducts.length; i += 3) {
+    productsChunks.push(featuredProducts.slice(i, i + 3));
+  }
+
+  return (
+    <Carousel id='carouselCards' activeIndex={startIndex} onSelect={handleSelect} className='mt-1'>
+      {productsChunks.map((chunk, index) => (
+        <Carousel.Item key={index}>
+          <div className="d-flex justify-content-around">
+            {chunk.map((product, productIndex) => (
+              <FeaturedCards key={productIndex} product={product} />
+            ))}
+          </div>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
 export default FeaturedCarousel;
