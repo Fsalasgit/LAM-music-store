db77236fc211d016bdcf83aceffa16da61618347
Franco Exequiel Salas <franco_10_65@hotmail.com>
2023-10-08 21:09:22 -0300

    a

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,43 +4,11 @@ import { FaRegHeart, FaHeart } from 'react-icons/fa';
 import { addCart } from '../context/GlobalActions';
 import { GlobalContext } from '../context/GlobalContext';
 
-
 const ProductCard = ({ product }) => {
-  const {  state, dispatch  } = useContext(GlobalContext);;
-  const [cartProducts, setCartProducts] = useState([]);;
-  const [isFavorite, setIsFavorite] = useState(false);const [isFavorite, setIsFavorite] = useState(false);
+  const { state, dispatch } = useContext(GlobalContext);
+  const [cartProducts, setCartProducts] = useState([]);
+  const [isFavorite, setIsFavorite] = useState(false);
 
-  useEffect(() => {
-    const favoritesFromLocalStorage = JSON.parse(localStorage.getItem('favorites')) || [];
-    setIsFavorite(favoritesFromLocalStorage.includes(product.id));
-  }, [product.id]);
-
-  const handleFavourite = () => {
-    const newFavorite = !isFavorite;
-    setIsFavorite(newFavorite);
-
-    const favoritesFromStorage = JSON.parse(localStorage.getItem('favorites')) || [];
-
-    const updatedFavorites = newFavorite
-      ? [...favoritesFromStorage, product._id]
-      : favoritesFromStorage.filter((id) => id !== product._id);
-
-    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
-  }
-
-  const onAddProduct = () => {
-    const existingProductIndex = state.productCart.findIndex((item) => item._id === product._id);
-
-    if (existingProductIndex !== -1) {
-      state.productCart[existingProductIndex].cantidad++;
-    } else {
-      const updatedCartProducts = [...cartProducts, { ...product, cantidad: 1 }];
-      setCartProducts(updatedCartProducts);
-      dispatch(addCart({ ...product, cantidad: 1 }));
-    }
-  };
-
-  const convertToPesos = (numb) => {
   useEffect(() => {
     const favoritesFromLocalStorage = JSON.parse(localStorage.getItem('favorites')) || [];
     setIsFavorite(favoritesFromLocalStorage.includes(product.id));
@@ -81,15 +49,17 @@ const ProductCard = ({ product }) => {
 
   return (
     <>
-      <div className='item col-3 m-3' key={product._id}>
-        <figure>
-          <img src={product.image} alt={product.title} />
-        </figure>
-        <div className='info-product'>
-          <h2>{product.title}</h2>
-          <p className='description'>{product.description}</p>
-          <p className='price'>{convertToPesos(product.price)}</p>
-          <button onClick={()=> onAddProduct(product)}>Añadir al carrito</button>
+      <Card className='productCard' key={product._id}>
+        <div className='productCard__header'>
+          {isFavorite ? (
+            <button className='productCard__favorite favorite' onClick={handleFavourite}>
+              <FaHeart className='productCard__favorite-icons' />
+            </button>
+          ) : (
+            <button className='productCard__favorite' onClick={handleFavourite}>
+              <FaRegHeart className='productCard__favorite-icons' />
+            </button>
+          )}
         </div>
         <caption className='productCard__imgContainer'>
           <Card.Img className='productCard__img' variant="top" src={product.image} alt={product.title} />
@@ -110,7 +80,7 @@ const ProductCard = ({ product }) => {
       </Card>
     </>
   );
-  }
+};
 
 export default ProductCard;
 
