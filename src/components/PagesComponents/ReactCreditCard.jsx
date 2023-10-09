477b94809a18b3543f36b4010011e8d3e144d228
Franco Exequiel Salas <franco_10_65@hotmail.com>
2023-10-09 20:45:18 -0300

    primer intro pago con tarjeta

--- src/components/PagesComponents/ReactCreditCard.jsx
+++ src/components/PagesComponents/ReactCreditCard.jsx
@@ -1,8 +1,102 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Cards from 'react-credit-cards-2';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+
 
 const ReactCreditCard = () => {
+    const [state, setState] = useState({
+        number: '',
+        expiry: '',
+        cvc: '',
+        name: '',
+        focus: '',
+      });
+    
+      const handleInputChange = (evt) => {
+        const { name, value } = evt.target;
+        
+        setState((prev) => ({ ...prev, [name]: value }));
+      }
+    
+      const handleInputFocus = (evt) => {
+        setState((prev) => ({ ...prev, focus: evt.target.name }));
+      }
   return (
     <>
+    <div>
+      <Cards
+        number={state.number}
+        expiry={state.expiry}
+        cvc={state.cvc}
+        name={state.name}
+        focused={state.focus}
+      />
+      <form className='mt-5'> 
+        <FloatingLabel
+            controlId="floatingInput"
+            label="Numero de tarjeta"
+            className="mb-3"
+        >
+            <Form.Control 
+            type="number"
+            name="number"
+            placeholder="Card Number"
+            value={state.number}
+            onChange={handleInputChange}
+            onFocus={handleInputFocus} 
+            />
+        </FloatingLabel>
+
+
+        <FloatingLabel
+            controlId="floatingInput"
+            label="Nombre del titular"
+            className="mb-3"
+        >
+            <Form.Control 
+            type="name"
+            name="name"
+            placeholder="Card name"
+            value={state.name}
+            onChange={handleInputChange}
+            onFocus={handleInputFocus} 
+            />
+        </FloatingLabel>
+        <FloatingLabel
+            controlId="floatingInput"
+            label="fecha de vencimiento"
+            className="mb-3 col-3"
+        >
+            <Form.Control 
+            type="expiry"
+            name="expiry"
+            placeholder="Card expiry"
+            value={state.expiry}
+            onChange={handleInputChange}
+            onFocus={handleInputFocus} 
+            />
+        </FloatingLabel>
+        <FloatingLabel
+            controlId="floatingInput"
+            label="fecha de vencimiento"
+            className="mb-3 col-3"
+        >
+            <Form.Control 
+            type="cvc"
+            name="cvc"
+            placeholder="Card cvc"
+            value={state.cvc}
+            onChange={handleInputChange}
+            onFocus={handleInputFocus} 
+            />
+        </FloatingLabel>
+
+      
+        
+      </form>
+    </div>
+
     </>
   )
 }
--- src/components/PaymentForms.jsx
+++ src/components/PaymentForms.jsx
@@ -4,11 +4,12 @@ import Tab from 'react-bootstrap/Tab';
 import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import ReactCreditCard from './PagesComponents/ReactCreditCard';
 
 const PaymentForms = ({ show, setShow }) => {
   return (
     <>
-      <Modal show={show} onHide={() => setShow(false)} dialogClassName='modal-90w' aria-labelledby='example-custom-modal-styling-title'>
+      <Modal show={show} onHide={() => setShow(false)} dialogClassName='modal-100W' aria-labelledby='example-custom-modal-styling-title'>
         <Modal.Header closeButton>
           <Modal.Title id='example-custom-modal-styling-title'>Realizar Pago</Modal.Title>
         </Modal.Header>
@@ -47,20 +48,9 @@ const PaymentForms = ({ show, setShow }) => {
                 <div>
                   <h2>Pago con Tarjeta</h2>
                   <Form>
+                    <ReactCreditCard />
                     
-                    <Form.Group controlId='formCardNumber'>
-                      <Form.Label>Número de Tarjeta</Form.Label>
-                      <Form.Control type='text' placeholder='Número de Tarjeta' />
-                    </Form.Group>
-                    <Form.Group controlId='formExpiration'>
-                      <Form.Label>Fecha de Expiración</Form.Label>
-                      <Form.Control type='text' placeholder='MM/YY' />
-                    </Form.Group>
-                    <Form.Group controlId='formCVV'>
-                      <Form.Label>CVV</Form.Label>
-                      <Form.Control type='text' placeholder='CVV' />
-                    </Form.Group>
-                   }
+                   
                   </Form>
                 </div>
               </Tab.Pane>
