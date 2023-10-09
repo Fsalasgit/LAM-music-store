094456c6d85248f9bfc73042f3aee4d195f5fd49
LorenzoAlbornoz <lorenzoa.28.la@gmail.com>
2023-10-09 11:34:59 -0300

    correcto funcionamiento de añadir al carrito, favorito y abrir detallado

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -50,49 +50,52 @@ const ProductCard = ({ product }) => {
 
   return (
     <>
-    <Link to={`/productos/${product._id}`} className='productCard'>
-      <Card key={product._id}>
-        <div className='productCard__header'>
-          {isFavorite ? (
-   <button
-   className='productCard__favorite favorite'
-   onClick={(e) => {
-     e.preventDefault(); // Previene la acción predeterminada del enlace
-     handleFavourite();
-   }}
- >
-   <FaHeart className='productCard__favorite-icons' />
- </button>
-) : (
- <button
-   className='productCard__favorite'
-   onClick={(e) => {
-     e.preventDefault(); // Previene la acción predeterminada del enlace
-     handleFavourite();
-   }}
- >
-   <FaRegHeart className='productCard__favorite-icons' />
- </button>
-          )}
-        </div>
-        <caption className='productCard__imgContainer'>
-          <Card.Img className='productCard__img' variant="top" src={product.image} alt={product.title} />
-        </caption>
-        <Card.Body className='productCard_body'>
-          <Card.Title className='productCard__title mt-1rem'>{product.title}</Card.Title>
-          <Card.Subtitle className='productCard__itemprice mt-1rem'>{convertToPesos(product.price)}</Card.Subtitle>
-          <Card.Text className='productCard__description mt-1rem'>
-            {product.description}
-          </Card.Text>
-        </Card.Body>
-        <div className='productCard__footer'>
-          <button onClick={onAddProduct} className='productCard__button'>Añadir al carrito</button>
+      <Link to={`/productos/${product._id}`} className='productCard'>
+        <Card key={product._id}>
+          <div className='productCard__header'>
+            {isFavorite ? (
+              <button
+                className='productCard__favorite favorite'
+                onClick={(e) => {
+                  e.preventDefault(); 
+                  handleFavourite();
+                }}
+              >
+                <FaHeart className='productCard__favorite-icons' />
+              </button>
+            ) : (
+              <button
+                className='productCard__favorite'
+                onClick={(e) => {
+                  e.preventDefault(); 
+                  handleFavourite();
+                }}
+              >
+                <FaRegHeart className='productCard__favorite-icons' />
+              </button>
+            )}
+          </div>
+          <caption className='productCard__imgContainer'>
+            <Card.Img className='productCard__img' variant="top" src={product.image} alt={product.title} />
+          </caption>
+          <Card.Body className='productCard_body'>
+            <Card.Title className='productCard__title mt-1rem'>{product.title}</Card.Title>
+            <Card.Subtitle className='productCard__itemprice mt-1rem'>{convertToPesos(product.price)}</Card.Subtitle>
+            <Card.Text className='productCard__description mt-1rem'>
+              {product.description}
+            </Card.Text>
+          </Card.Body>
+          <div className='productCard__footer'>
+            <button onClick={(e) => {
+              e.preventDefault()
+              onAddProduct()
+            }} 
+              className='productCard__button'>Añadir al carrito</button>
+          </div>
 
-        </div>
 
-        
-      </Card>
-    </Link>
+        </Card>
+      </Link>
     </>
   );
 };
@@ -102,4 +105,4 @@ export default ProductCard;
 
 
 
-  
+
