7b4a09b7f2f04a4c48e6998bf770a19b49affd62
LorenzoAlbornoz <lorenzoa.28.la@gmail.com>
2023-10-11 22:52:43 -0300

    Orden de sass

--- src/components/PagesComponents/ProgressBar3.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ProgressBar from 'react-bootstrap/ProgressBar';
-
-function ProgressBar3() {
-  const now = "REACT&JS";
-  return <ProgressBar variant="warning" animated now={70} label={`${now}`} />
-  }
-  
-export default ProgressBar3;
