e418d341509b3726ea2c8d2fca473af0431ef03f
LorenzoAlbornoz <lorenzoa.28.la@gmail.com>
2023-10-11 23:01:01 -0300

    modificaciones carrito

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -55,21 +55,7 @@ const Carrito = () => {
                         <p className='titulo-producto-carrito'>{product.title}</p>
                         <span className='precio-producto-carrito'>{convertToPesos(product.price)}</span>
                       </div>
-                      <svg
-							xmlns='http://www.w3.org/2000/svg'
-							fill='none'
-							viewBox='0 0 24 24'
-							strokeWidth='1.5'
-							stroke='currentColor'
-							className='icon-close'
-							onClick={() => onDeleteProduct(product._id)}
-						>
-							<path
-								strokeLinecap='round'
-								strokeLinejoin='round'
-								d='M6 18L18 6M6 6l12 12'
-							/>
-						</svg>
+
                     </div>
                   ))}
                 </div>
